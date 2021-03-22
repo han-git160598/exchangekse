f406a1a97f0facb6828e211bd0d5fe385762b06b
han-git160598 <58239643+han-git160598@users.noreply.github.com>
2021-03-22 10:28:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ function random_y(number)
   var visits=23000;
   setInterval(function () {
   var y = random_y(visits);
+   visits = y;
   var x = Math.floor((new Date().getTime())/1000);
   var xy = {x:x, y:y};
   var coordinate_xy = JSON.stringify(xy);
